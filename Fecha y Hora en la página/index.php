886763c5f0efed1399942a4d9dd17d5f8da239f4
Fryntiz <tecnico@fryntiz.es>
2016-10-04 12:03:14 +0200

    Programa completo con varios ejemplos

--- Fecha y Hora en la página/index.php
+++ Fecha y Hora en la página/index.php
@@ -12,14 +12,26 @@
 </HEAD>
 
 <BODY>
-	<H1 id="titulo1">Título de la página</H1>
-	<H2 id="titulo2">Sección de que se trata</H2>
-	<P>Descripción</P>
-	<DIV class="ejemplo">
-		Plantilla de ejemplo 
+	<H1 id="titulo1">FECHA Y HORA</H1>
+	<H2 id="titulo2">Apliación que muestra la hora y Fecha</H2>
+	<P>En esta aplicación se muestra la fecha y hora, cosa que puede ser necesaria incluir en nuestro sitio web</P>
+	<DIV class="ejemplo"><!--Recuadro central verde-->
+		<?php
+		echo "Hoy es"." ",date("d/m/Y"),"y la hora actual es"." ",date("h:i:s"),".<BR/><BR/>Queremos daros la bienvenida a nuestra WEB.";
+		echo "<BR/><BR/>";
+		echo "Otro Ejemplo más corto:<BR/>";
+		echo date("d/m/Y")," - ".date("h:i");
+		?>
 	</DIV>
-<?php
-
-?>
+	
+	<DIV class="recuadro"><!--Recuadro fijo abajo y a la izquierda-->
+		<?php
+		echo date("h:i");
+		echo "<BR/>";
+		echo date("d/m");
+		echo "<BR/>";
+		echo date("Y");
+		?>
+	</DIV>
 </BODY>
 </HTML>
