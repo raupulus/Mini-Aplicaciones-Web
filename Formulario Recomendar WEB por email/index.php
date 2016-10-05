ce5475409f40b8206d8416e47ce41595359a2060
Fryntiz <tecnico@fryntiz.es>
2016-10-05 15:38:41 +0200

    Creado formulario de entrada

--- Formulario Recomendar WEB por email/index.php
+++ Formulario Recomendar WEB por email/index.php
@@ -16,6 +16,28 @@
 	<H2 id="titulo2">Recomienda la página web a un amigo</H2>
 	<P>Este formulario procesará los datos introducidos y enviará una recomendación del sitio a un amigo</P>
 	<DIV class="ejemplo">
+		<H3>Recomienda este sitio a un amigo:</H3>
+		<FORM method="POST" action="procesar.php">
+			<P>
+				Tu nombre:
+				<BR/>
+				<INPUT type="text" name="tunombre" size=20/>
+				<BR/>
+
+				Tu email:
+				<BR/>
+				<INPUT type="text" name="tuemail" size="20"/>
+				<BR/>
+
+				Nombre de tu amigo:
+				<BR/>
+				<INPUT type="text" name="emailamigo" size="20"/>
+				<BR/><BR/>
+
+				<INPUT type="submit" value="Recomendar" name="B1"/>
+			</P>
+		</FORM>
+		
 		<?php
 		
 		?>
