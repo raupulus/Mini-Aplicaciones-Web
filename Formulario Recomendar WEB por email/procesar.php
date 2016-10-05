e88b5f1909fac059fad85e014e37a12a4db4d3d8
Fryntiz <tecnico@fryntiz.es>
2016-10-05 15:38:28 +0200

    Base para procesar el formulario a falta del programa

--- Formulario Recomendar WEB por email/procesar.php
+++ Formulario Recomendar WEB por email/procesar.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <HTML lang="es">
 <HEAD>
-	<TITLE>Nombre</TITLE>
+	<TITLE>Formulario recomendar WEB</TITLE>
 	<META charset="utf-8"/>
-	<META name="description" content="Plantilla de Ejemplo"/>
-	<META name="keywords" content="ejemplo, plantilla, programación, Raúl Caro Pastorino, Fryntiz"/>
+	<META name="description" content="Formulario recomendar WEB"/>
+	<META name="keywords" content="formulario, mail, email, recomendar, web, ejemplo, plantilla, programación, Raúl Caro Pastorino, Fryntiz"/>
 	<META name="author" content="Raúl Caro Pastorino"/>
 	<LINK rel="shortcut icon" href="./images/favicon.png"/>
 	<LINK rel="stylesheet" href="./CSS/styles.css"/>
@@ -12,14 +12,8 @@
 </HEAD>
 
 <BODY>
-	<H1 id="titulo1">Título de la página</H1>
-	<H2 id="titulo2">Sección de que se trata</H2>
-	<P>Descripción</P>
-	<DIV class="ejemplo">
-		Plantilla de ejemplo
-	</DIV>
-<?php
-
-?>
+	<?php
+	
+	?>
 </BODY>
 </HTML>
