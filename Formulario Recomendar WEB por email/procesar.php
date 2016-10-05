c1c83cd15d61cc2651ff404591654321e404401c
Fryntiz <tecnico@fryntiz.es>
2016-10-05 16:05:34 +0200

    Añadidas variables obtenidas mediante POST

--- Formulario Recomendar WEB por email/procesar.php
+++ Formulario Recomendar WEB por email/procesar.php
@@ -14,13 +14,18 @@
 <BODY>
 	<DIV class="ejemplo">
 		<?php
+		//Variables pasadas mediante POST
+		$tunombre = $_POST['tunombre'];
+		$tuemail = $_POST['tuemail'];
+		$nombreamigo = $_POST['nombreamigo'];
+		$emailamigo = $_POST['emailamigo'];
 		
 		//Variables con el Contenido del mensaje
 		$asunto = "Te recomiendo visitar este portal.";
-		$mensaje = "Hola $nombreamigo, soy $tunombre y te recomiendo visitar www.fryntiz.es, un portal en el que podrás conocer más sobre el programador que ha realizado esto. Espero que lo visites pronto y estoy convencido que te va a gustar. Por cierto, si es de tu agrado, comparte ese sitio web con más personas que le pueda interesar.";
+		$mensaje = "Hola <B>$nombreamigo</B>, soy <B>$tunombre</B> y te recomiendo visitar <B style='color:red;font-size:1.2;'>www.fryntiz.es</B>, un portal en el que podrás conocer más sobre el programador que ha realizado esto. Espero que lo visites pronto y estoy convencido que te va a gustar. Por cierto, si es de tu agrado, comparte ese sitio web con más personas que le pueda interesar.";
 
 		//Función debug para comprobar que llegan bien todas las variables
-		function debugMAIL() {
+		function debugMAIL($emailamigo,$asunto,$mensaje,$tuemail) {
 			echo "Asunto: $asunto";
 			echo "<BR/>";
 			echo "Mensaje: $mensaje";
@@ -30,12 +35,13 @@
 			echo "Tu email: $tuemail";
 		}
 
-		debugMAIL();
+		debugMAIL($emailamigo,$asunto,$mensaje,$tuemail);
 		
-		function enviarMAIL() {
+		function enviarMAIL($emailamigo,$asunto,$mensaje,$tuemail) {
 			mail($emailamigo,$asunto,$mensaje, "FROM: ".$tuemail);
 		}
-		//enviarMAIL();
+		
+		//enviarMAIL($emailamigo,$asunto,$mensaje,$tuemail);
 		?>
 	</DIV>
 </BODY>
