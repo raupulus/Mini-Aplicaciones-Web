8a7d750c69ea9e1f00afd3d4825daefe228401e6
Fryntiz <tecnico@fryntiz.es>
2016-10-05 15:48:38 +0200

    Base para procesar formulario y enviar correo. De momento solo para debug datos pasados

--- Formulario Recomendar WEB por email/procesar.php
+++ Formulario Recomendar WEB por email/procesar.php
@@ -13,7 +13,19 @@
 
 <BODY>
 	<?php
+	$asunto = "Te recomiendo visitar este portal.";
+	$mensaje = "Hola $nombreamigo, soy $tunombre y te recomiendo visitar www.fryntiz.es, un portal en el que podrás conocer más sobre el programador que ha realizado esto. Espero que lo visites pronto y estoy convencido que te va a gustar. Por cierto, si es de tu agrado, comparte ese sitio web con más personas que le pueda interesar.";
 	
+	function debugMAIL() {
+		echo "Asunto: $asunto";
+		echo "<BR/>";
+		echo "Mensaje: $mensaje";
+		echo "<BR/>";
+		echo "Email de amigo: $emailamigo";
+		echo "Tu email: $tuemail";
+	}
+	
+	debugMAIL();
 	?>
 </BODY>
 </HTML>
