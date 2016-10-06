585bcc84afb72168decb4992f726dbdb5ed4d422
Fryntiz <tecnico@fryntiz.es>
2016-10-06 16:51:31 +0200

    Renombrado directorio

---
+++ Proteger páginas por contraseña sin BD/comprobar.php
@@ -0,0 +1,18 @@
+		<?php
+		$usuario = $_POST['usuario'];
+		$pass = $_POST['pass'];
+		
+		if($usuario=="usuario" && $pass=="clave") {
+			$valido = "si";
+		} else {
+			$valido = "no";
+		}
+		if ($valido == "si") {
+			echo "<SCRIPT type='text/javascript'>alert('Bienvenido, has introducido correctamente el usuario y la contraseña.')</SCRIPT>";
+			//header("Location: webprotegida.php");
+			include "webprotegida.php";
+		} else {
+			//echo "Vuelve a intentarlo:<BR/>El usuario o la contraseña son incorrectos.";
+			include "error.php";
+		}
+		?>
