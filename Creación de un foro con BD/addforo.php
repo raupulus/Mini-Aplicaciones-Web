06438b907a00297affde9e68cd27edd5a76d3ff2
Fryntiz <tecnico@fryntiz.es>
2016-10-07 00:39:39 +0200

    Vista previa de mensaje enviado

---
+++ Creación de un foro con BD/addforo.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<HTML lang="es">
+<HEAD>
+	<TITLE>Foro</TITLE>
+	<META charset="utf-8"/>
+	<META name="description" content="Foro"/>
+	<META name="keywords" content="foro, ejemplo, plantilla, programación, Raúl Caro Pastorino, Fryntiz"/>
+	<META name="author" content="Raúl Caro Pastorino"/>
+	<LINK rel="shortcut icon" href="./images/favicon.png"/>
+	<LINK rel="stylesheet" href="./CSS/styles.css"/>
+	<SCRIPT src="JS/scripts.js"></SCRIPT>
+</HEAD>
+
+<BODY>
+	<H1 id="titulo1">Vista previa</H1>
+	<H2 id="titulo2">Revisa el mensaje antes de publicarlo</H2>
+	<P>Desde aquí puedes comprobar si es correcto lo que quieres enviar</P>
+	<DIV class="ejemplo">
+		
+	</DIV>
+</BODY>
+</HTML>
