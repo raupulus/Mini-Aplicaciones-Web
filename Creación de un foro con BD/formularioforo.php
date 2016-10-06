4b2aa1ef6fcf178c5543531639bea9bba7ac2a6e
Fryntiz <tecnico@fryntiz.es>
2016-10-06 18:36:05 +0200

    Creada base para el formulario donde se añade nuevo mensaje

---
+++ Creación de un foro con BD/formularioforo.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<HTML lang="es">
+<HEAD>
+	<TITLE>Foro</TITLE>
+	<META charset="utf-8"/>
+	<META name="description" content="Foro"/>
+	<META name="keywords" content="foro, ejemplo, plantilla, programación, Raúl Caro Pastorino, Fryntiz"/>
+	<META name="author" content="Raúl Caro Pastorino"/>
+	<LINK rel="shortcut icon" href="./images/favicon.png"/>
+	<LINK rel="stylesheet" href="./CSS/styles.css"/>
+	<SCRIPT src="JS/scripts.js"></SCRIPT>
+</HEAD>
+
+<BODY>
+	<H1 id="titulo1">Nuevo Mensaje</H1>
+	<H2 id="titulo2">Añadir una nueva respuesta</H2>
+	<P>Rellena el siguiente formulario para añadir un mensaje</P>
+	<DIV>
+		
+	</DIV>
+
+</BODY>
+</HTML>
