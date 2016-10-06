ae5e23cb15383e90eaee1bf812de4db0dc4c2129
Fryntiz <tecnico@fryntiz.es>
2016-10-06 17:19:41 +0200

    HTML básico para un Foro

---
+++ Creación de un foro con BD/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<HTML lang="es">
+<HEAD>
+	<TITLE>Foro</TITLE>
+	<META charset="utf-8"/>
+	<META name="description" content="Foro"/>
+	<META name="keywords" content="foro, ejemplo, plantilla, programación, Raúl Caro Pastorino, Fryntiz"/>
+	<META name="author" content="Raúl Caro Pastorino"/>
+	<LINK rel="shortcut icon" href="./images/favicon.png"/>
+	<LINK rel="stylesheet" href="./CSS/styles.css"/>
+	<SCRIPT src="JS/scripts.js"></SCRIPT>
+</HEAD>
+
+<BODY>
+	<H1 id="titulo1">Foro</H1>
+	<H2 id="titulo2">Esto es un ejemplo de foro</H2>
+	<P>Descripción</P>
+	<DIV class="ejemplo">
+		Plantilla de ejemplo
+	</DIV>
+<?php
+
+?>
+</BODY>
+</HTML>
