736d5dc034386e9041004313c6b08771d169b171
Fryntiz <tecnico@fryntiz.es>
2016-10-06 11:58:02 +0200

    Dejo limpio el código y meto mediante include el nuevo sitio

--- Proteger páginas por contraseña/comprobar.php
+++ Proteger páginas por contraseña/comprobar.php
@@ -1,18 +1,3 @@
-<!DOCTYPE html>
-<HTML lang="es">
-<HEAD>
-	<TITLE>Proteger página por contraseña</TITLE>
-	<META charset="utf-8"/>
-	<META name="description" content="Proteger página por contraseña"/>
-	<META name="keywords" content="proteger, contraseña, pagina, proteger pagina, ejemplo, plantilla, programación, Raúl Caro Pastorino, Fryntiz"/>
-	<META name="author" content="Raúl Caro Pastorino"/>
-	<LINK rel="shortcut icon" href="./images/favicon.png"/>
-	<LINK rel="stylesheet" href="./CSS/styles.css"/>
-	<SCRIPT src="JS/scripts.js"></SCRIPT>
-</HEAD>
-
-<BODY>
-	<DIV class="ejemplo">
 		<?php
 		$usuario = $_POST['usuario'];
 		$pass = $_POST['pass'];
@@ -23,11 +8,11 @@
 			$valido = "no";
 		}
 		if ($valido == "si") {
-			echo "Bienvenido, has introducido correctamente el usuario y la contraseña.";
+			echo "<SCRIPT type='text/javascript'>alert('Bienvenido, has introducido correctamente el usuario y la contraseña.')</SCRIPT>";
+			//header("Location: webprotegida.php");
+			include "webprotegida.php";
 		} else {
-			echo "Vuelve a intentarlo: El usuario o la contraseña son incorrectos.";
+			//echo "Vuelve a intentarlo:<BR/>El usuario o la contraseña son incorrectos.";
+			include "error.php";
 		}
 		?>
-	</DIV>
-</BODY>
-</HTML>
