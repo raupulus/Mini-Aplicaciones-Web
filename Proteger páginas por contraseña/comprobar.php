96b85cf84e6d076d703dfe0b28518d90a82c58a9
Fryntiz <tecnico@fryntiz.es>
2016-10-06 10:07:53 +0200

    base para comprobar si es correcto usuario y contraseña

---
+++ Proteger páginas por contraseña/comprobar.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<HTML lang="es">
+<HEAD>
+	<TITLE>Proteger página por contraseña</TITLE>
+	<META charset="utf-8"/>
+	<META name="description" content="Proteger página por contraseña"/>
+	<META name="keywords" content="proteger, contraseña, pagina, proteger pagina, ejemplo, plantilla, programación, Raúl Caro Pastorino, Fryntiz"/>
+	<META name="author" content="Raúl Caro Pastorino"/>
+	<LINK rel="shortcut icon" href="./images/favicon.png"/>
+	<LINK rel="stylesheet" href="./CSS/styles.css"/>
+	<SCRIPT src="JS/scripts.js"></SCRIPT>
+</HEAD>
+
+<BODY>
+	<DIV class="ejemplo">
+		<?php
+		
+		?>
+	</DIV>
+</BODY>
+</HTML>
