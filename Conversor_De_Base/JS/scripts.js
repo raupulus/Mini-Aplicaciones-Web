48719bdccf45648ffa81117fcee98a8968cb322c
Fryntiz <tecnico@fryntiz.es>
2016-11-07 03:21:01 +0100

    Añadido obtener valor del primer select y planteado proximos pasos

--- Conversor_De_Base/JS/scripts.js
+++ Conversor_De_Base/JS/scripts.js
@@ -9,4 +9,33 @@ function accion() {
 		return false;
 	}
 
+	//Obtener Valor de Select de base de entrada
+	var lista = document.getElementById("selectEntrada");
+	var indiceSeleccionado = lista.selectedIndex;
+	var opcionSeleccionada = lista.options[indiceSeleccionado];
+	var valorSeleccionado = opcionSeleccionada.value;
+
+	//Obtener Valor de Select de base de salida
+
+
+
+
+	alert();
+
+
+
+	/*
+	x = parseInt(x);
+	var bin = x.toString(2);
+	var hex = x.toString(16).toUpperCase();
+	var octal = x.toString(8);
+
+
+	var figs = "The binary representation of " + x + " is " + bin + "<br>";
+	figs = figs + "The hexadecimal representation of " + x + " is " + hex + "<br>";
+	figs = figs + "The octal representation of " + x + " is " + octal + "<br>";
+
+	document.getElementById("result").innerHTML = figs;
+}
+*/
 }
