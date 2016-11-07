311dc47a3b0d018c224d7470b154668cb535e797
Fryntiz <tecnico@fryntiz.es>
2016-11-07 02:14:29 +0100

    Conversor de Base index

---
+++ Conversor_De_Base/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<HTML lang="es">
+<HEAD>
+	<TITLE>Nombre</TITLE>
+	<META charset="utf-8"/>
+	<META name="description" content="Plantilla de Ejemplo"/>
+	<META name="keywords" content="ejemplo, plantilla, programación, Raúl Caro Pastorino, Fryntiz"/>
+	<META name="author" content="Raúl Caro Pastorino"/>
+	<LINK rel="shortcut icon" href="./images/favicon.png"/>
+	<LINK rel="stylesheet" href="./CSS/styles.css"/>
+	<SCRIPT src="JS/scripts.js"></SCRIPT>
+</HEAD>
+
+<BODY>
+	<H1 id="titulo1">Conversor de Base</H1>
+	<H2 id="titulo2">Convierte entre bases</H2>
+	<P>Utiliza la siguiente calculadora para convertir a la base que necesites</P>
+	<DIV class="ejemplo">
+
+	</DIV>
+<?php
+
+?>
+</BODY>
+</HTML>
