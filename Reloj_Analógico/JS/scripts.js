4c399ae6e6a9dc3c576fc38b5df132a0686b9fe3
Fryntiz <tecnico@fryntiz.es>
2016-12-12 12:05:41 +0100

    Función para rotar automáticamente el reloj

--- Reloj_Analógico/JS/scripts.js
+++ Reloj_Analógico/JS/scripts.js
@@ -1,6 +1,19 @@
 function funcionReloj() {
+	momentoActual = new Date();//Crea hora
+	horaActual = momentoActual.getHours();//Extraemos Hora
+	minutoActual = momentoActual.getMinutes();//Extraemos Minutos
+
 	hora = document.getElementById('hora');
 	minutos = document.getElementById('minutos');
 
+//Calcular grados cada minuto (6deg cada minuto, 6*6=360º)
+	minutoActual = minutoActual * 6 + "deg";
+	minutos.style.transform = "rotate(" + minutoActual + ")";
 
+//Calcular grados por hora (30deg cada hora, 30*12=360º)
+	horaActual = horaActual * 30 + "deg";
+	hora.style.transform = "rotate(" + horaActual + ")";
+
+//Actualizar reloj cada minuto
+	setTimeout("cambiarHora()",60000);
 }
