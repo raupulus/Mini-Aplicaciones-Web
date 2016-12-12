ecb633a5f3818d86a871e4749deca8a1a27e6546
Fryntiz <tecnico@fryntiz.es>
2016-12-12 12:37:44 +0100

    añadidos segundos al reloj analogico

--- Reloj_Analógico/JS/scripts.js
+++ Reloj_Analógico/JS/scripts.js
@@ -1,19 +1,35 @@
 function funcionReloj() {
-	momentoActual = new Date();//Crea hora
-	horaActual = momentoActual.getHours();//Extraemos Hora
-	minutoActual = momentoActual.getMinutes();//Extraemos Minutos
+	var momentoActual = new Date();//Crea hora
+	var horaActual = momentoActual.getHours();//Extraemos Hora
+	var minutoActual = momentoActual.getMinutes();//Extraemos Minutos
+	var segundoActual = momentoActual.getSeconds();//Extraemos Segundos
 
-	hora = document.getElementById('hora');
-	minutos = document.getElementById('minutos');
+//Identificadores para elementos
+	var hora = document.getElementById('hora');
+	var minutos = document.getElementById('minutos');
+	var segundos = document.getElementById('segundos');
 
-//Calcular grados cada minuto (6deg cada minuto, 6*6=360º)
-	minutoActual = minutoActual * 6 + "deg";
-	minutos.style.transform = "rotate(" + minutoActual + ")";
+	function cambiarHora() {
+	//Calcular grados por hora (30deg cada hora, 30*12=360º)
+		var horaActualGrados = horaActual * 30 + "deg";
+		hora.style.transform = "rotate(" + horaActualGrados + ")";
 
-//Calcular grados por hora (30deg cada hora, 30*12=360º)
-	horaActual = horaActual * 30 + "deg";
-	hora.style.transform = "rotate(" + horaActual + ")";
+	//Calcular grados cada minuto (6deg cada minuto, 6*6=360º)
+		var minutoActualGrados = minutoActual * 6 + "deg";
+		minutos.style.transform = "rotate(" + minutoActualGrados + ")";
+	}
 
-//Actualizar reloj cada minuto
-	setTimeout("cambiarHora()",60000);
+	function cambiarSegundos() {
+	//Calcular grados cada segundo (6deg cada segundo, 6*6=360º)
+		var segundoActualGrados = segundoActual * 6 + "deg";
+		segundos.style.transform = "rotate(" + segundoActualGrados + ")";
+	}
+
+cambiarHora();
+cambiarSegundos();
+
+//Actualizar reloj
+	setTimeout("funcionReloj()",1000);
+//Actualizar reloj cada segundo
+//	setTimeout("cambiarSegundos()",1000);
 }
