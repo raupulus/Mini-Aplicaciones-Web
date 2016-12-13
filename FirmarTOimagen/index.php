22530359e115b4abd1e6aae82ea9feb16938ea95
Fryntiz <tecnico@fryntiz.es>
2016-12-13 13:39:41 +0100

    archivo base php para mini aplicacion de firmar

---
+++ FirmarTOimagen/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<HTML lang="es">
+<HEAD>
+	<TITLE>Firmar</TITLE>
+	<META charset="utf-8"/>
+	<META name="description" content="Formulario recomendar WEB"/>
+	<META name="keywords" content="firmar, firma, formulario, mail, email, recomendar, web, ejemplo, plantilla, programación, Raúl Caro Pastorino, Fryntiz"/>
+	<META name="author" content="Raúl Caro Pastorino"/>
+	<LINK rel="shortcut icon" href="./images/favicon.png"/>
+	<LINK rel="stylesheet" href="./CSS/styles.css"/>
+	<SCRIPT src="JS/scripts.js"></SCRIPT>
+</HEAD>
+
+<BODY>
+
+</BODY>
+</HTML>
