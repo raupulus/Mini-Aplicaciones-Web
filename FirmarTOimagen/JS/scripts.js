c303c024da66d321a08cabdfb0972b4f9c5ad148
Fryntiz <tecnico@fryntiz.es>
2016-12-14 00:47:01 +0100

    Funciones javascripts terminadas para aplicacion guardar firma

--- FirmarTOimagen/JS/scripts.js
+++ FirmarTOimagen/JS/scripts.js
@@ -1,9 +1,20 @@
+function mostrarLienzo(mostrar) {
+	formulario = document.getElementById('formFirma');
+	if (mostrar) {
+		formulario.style.display = "block";
+		comenzar(); //Empieza a escuchar eventos
+	} else {
+		formulario.style.display = "none";
+		document.remove
+	}
+
+}
+
 function comenzar() {
-	lienzo = document.getElementById('lienzo');
+	lienzo = document.getElementById('lienzo'); //El lienzo
 	ctx = lienzo.getContext('2d');
-//Dejamos todo preparado para escuchar los eventos
+//Escuchar eventos (movimientos y pulsaciones)
 	document.addEventListener('mousedown',pulsaRaton,false); document.addEventListener('mousemove',mueveRaton,false); document.addEventListener('mouseup',levantaRaton,false);
-	ctx.fillRect (400, 0, 400, 200);
 }
 
 function pulsaRaton(capturo) {
@@ -16,10 +27,12 @@ function pulsaRaton(capturo) {
 
 function mueveRaton(capturo) {
 	if(estoyDibujando){
-//indicamos el color de la línea
+//Color de la brocha
 		ctx.strokeStyle='red';
 //Por dónde vamos dibujando
 		ctx.lineTo(capturo.clientX,capturo.clientY);
+		ctx.lineJoin = 'round';
+		ctx.lineWidth = 3;//Ancho de línea
 		ctx.stroke();
 	}
 }
@@ -35,3 +48,22 @@ function limpiando() {
 	lienzo = document.getElementById('lienzo');
 	ctx.clearRect(0, 0, lienzo.width, lienzo.height);
 }
+
+function guardarImagen() {
+	lienzo = document.getElementById('lienzo');
+	data = lienzo.toDataURL('image/png');
+	xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      // Completado y abriendo en nueva pestaña
+      if (xhr.readyState == 4) {
+        window.open('./firmas/'+xhr.responseText,'_blank');
+      }
+    }
+	xhr.open('POST','guardarImagen.php',true);
+	xhr.setRequestHeader('Content-Type', 'application/upload');
+	xhr.send(data);
+
+	//Salir de firmar
+	limpiando();
+	mostrarLienzo(false);
+ }
