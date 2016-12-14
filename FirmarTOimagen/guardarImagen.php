bf30157ba1d02ded00ed47087bfbf7bc441f001c
Fryntiz <tecnico@fryntiz.es>
2016-12-14 01:01:09 +0100

    Al guardar la firma ahora se sigue un orden según la cantidad de archivos del directorio firmas (antes era aleatorio el nombre)

--- FirmarTOimagen/guardarImagen.php
+++ FirmarTOimagen/guardarImagen.php
@@ -9,11 +9,13 @@
 	$decodedData=base64_decode($filteredData);
 	// Guardar al servidor
 	$NombreFirma = 'firma'.rand(1000,9999).'.png';
-	$fp = fopen('./firmas/'.$NombreFirma, 'wb');
+	$ssss = count(glob("firmas/{*.jpg,*.gif,*.png}",GLOB_BRACE));
+	$ssss = "Firma".($ssss + 1).".png";
+	$fp = fopen('./firmas/'.$ssss, 'wb');
 	$ok = fwrite( $fp, $decodedData);
 	fclose( $fp );
 	if($ok)
-		echo $NombreFirma;
+		echo $ssss;
 	else
 		echo "ERROR";
 ?>
