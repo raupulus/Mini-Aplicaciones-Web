52d60b15794dd884de4bfc00d7800cfd83a19c1d
Fryntiz <tecnico@fryntiz.es>
2016-12-15 20:28:44 +0100

    HTML estructura básica probando

---
+++ Mostrar_Menú_con_Hover/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<HTML>
+	<HEAD>
+		<TITLE>Mostrar Menú con Hover</TITLE>
+		<META charset="utf-8"/>
+		<LINK rel="stylesheet" type="text/css" href="./CSS/styles.css"/>
+	</HEAD>
+
+	<BODY>
+		<NAV id="MenuPrincipal">
+			<UL>
+				<LI>
+					<P id>Tapa</P>
+					<P>Menú 1</P>
+				</LI>
+				<LI>
+					<P>Menú 1</P>
+				</LI>
+				<LI>
+					<P>Menú 1</P>
+				</LI>
+				<LI>
+					<P>Menú 1</P>
+				</LI>
+			</UL>
+		</NAV>
+	</BODY>
+</HTML>
