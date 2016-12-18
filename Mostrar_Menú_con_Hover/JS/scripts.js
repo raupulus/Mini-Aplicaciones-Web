aa518131911e369dfb81cc4ea7e068864f530cb7
Fryntiz <tecnico@fryntiz.es>
2016-12-18 18:44:33 +0100

    script para menú

---
+++ Mostrar_Menú_con_Hover/JS/scripts.js
@@ -0,0 +1,9 @@
+function ocultar(elemento) {
+	var clase = document.getElementsByClassName("tapa");
+	clase[elemento].style.display = "none";
+}
+
+function mostrar(elemento) {
+	var clase = document.getElementsByClassName("tapa");
+	clase[elemento].style.display = "block";
+}
