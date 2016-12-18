15f59357ac8a66e78ccd231fe322384f1d49c571
Fryntiz <tecnico@fryntiz.es>
2016-12-18 18:46:05 +0100

    estructura para menú hover

--- Mostrar_Menú_con_Hover/index.php
+++ Mostrar_Menú_con_Hover/index.php
@@ -4,23 +4,28 @@
 		<TITLE>Mostrar Menú con Hover</TITLE>
 		<META charset="utf-8"/>
 		<LINK rel="stylesheet" type="text/css" href="./CSS/styles.css"/>
+		<LINK rel="stylesheet" href="./CSS/styles.css"/>
+		<SCRIPT src="./JS/scripts.js"></SCRIPT>
 	</HEAD>
 
 	<BODY>
 		<NAV id="MenuPrincipal">
 			<UL>
 				<LI>
-					<P id>Tapa</P>
-					<P>Menú 1</P>
+					<P class="tapa" onMouseOver="ocultar('0')">1</P>
+					<P class="opciones" onMouseOut="mostrar('0')">Opción 1</P>
 				</LI>
 				<LI>
-					<P>Menú 1</P>
+					<P class="tapa" onMouseOver="ocultar('1')">2</P>
+					<P class="opciones" onMouseOut="mostrar('1')">Opción 2</P>
 				</LI>
 				<LI>
-					<P>Menú 1</P>
+					<P class="tapa" onMouseOver="ocultar('2')">3</P>
+					<P class="opciones" onMouseOut="mostrar('2')">Opción 3</P>
 				</LI>
 				<LI>
-					<P>Menú 1</P>
+					<P class="tapa" onMouseOver="ocultar('3')">4</P>
+					<P class="opciones" onMouseOut="mostrar('3')">Opción 4</P>
 				</LI>
 			</UL>
 		</NAV>
