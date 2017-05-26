4379cb508d5d3123cec8738fd1a490677b4c3bea
Fryntiz <tecnico@fryntiz.es>
2017-05-26 18:17:49 +0200

    Control de contenido vacío

--- Formulario Enviar Sugerencias/JS/scripts.js
+++ Formulario Enviar Sugerencias/JS/scripts.js
@@ -5,13 +5,27 @@ function ValidarFormulario() {
 	
 	//Solo permitir a-Z
 	//nombre
-	
+	if (nombre.value.length == 0) { 
+		alert("Tiene que escribir su nombre") 
+		nombre.focus() 
+		return false; 
+	} 
 	
 	//email
 	//Permitir a-Z0-9
+	if (email.value.length == 0) { 
+		alert("El email no debe estar vacío") 
+		email.focus() 
+		return false; 
+	} 
 	
 	//sugerencia
 	//Permitir a-Z0-9
+	if (sugerencia.value.length==0) { 
+		alert("No puede enviar una sugerencia vacía") 
+		sugerencia.focus() 
+		return false; 
+	} 
 	
-	return false;
+	return true;
 }
