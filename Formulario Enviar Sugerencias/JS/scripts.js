daf21a90f64b4770321282492126279bd7ccb34e
Fryntiz <tecnico@fryntiz.es>
2017-05-26 19:32:29 +0200

    Reparado control de errores por cadenas vacías

--- Formulario Enviar Sugerencias/JS/scripts.js
+++ Formulario Enviar Sugerencias/JS/scripts.js
@@ -1,31 +1,33 @@
-function ValidarFormulario() {
-	var nombre = document.FormSugerencia.nombre.value;
-	var email = document.FormSugerencia.email.value;
-	var sugerencia = document.FormSugerencia.sugerencia.value;
+function validarFormulario() {
+	var nombre = document.FormSugerencia.nombre;
+	var email = document.FormSugerencia.email;
+	var sugerencia = document.FormSugerencia.sugerencia;
 	
+
 	//Solo permitir a-Z
 	//nombre
 	if (nombre.value.length == 0) { 
-		alert("Tiene que escribir su nombre") 
-		nombre.focus() 
+		alert("Tiene que escribir su nombre");
+		nombre.focus();
 		return false; 
 	} 
 	
 	//email
 	//Permitir a-Z0-9
 	if (email.value.length == 0) { 
-		alert("El email no debe estar vacío") 
-		email.focus() 
+		alert("El email no debe estar vacío");
+		email.focus();
 		return false; 
 	} 
 	
 	//sugerencia
 	//Permitir a-Z0-9
-	if (sugerencia.value.length==0) { 
-		alert("No puede enviar una sugerencia vacía") 
-		sugerencia.focus() 
+	if (sugerencia.value.length == 0) { 
+		alert("No puede enviar una sugerencia vacía");
+		sugerencia.focus();
 		return false; 
 	} 
-	
-	return true;
+
+	//alert(nombre.value.length);
+	//return true;
 }
