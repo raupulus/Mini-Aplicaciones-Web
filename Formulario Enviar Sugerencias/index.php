a0c15f8f8cebe90087e2f74ca3569a82eddcec8f
Fryntiz <tecnico@fryntiz.es>
2017-05-26 17:02:11 +0200

    Campos del formulario establecidos

--- Formulario Enviar Sugerencias/index.php
+++ Formulario Enviar Sugerencias/index.php
@@ -16,10 +16,12 @@
 	<H2 id="titulo2">Sección de que se trata</H2>
 	<P>Descripción</P>
 	<DIV class="ejemplo">
-		<FORM method="POST" action="procesar.php">
+		<FORM id="FormSugerencia" method="POST" action="procesar.php">
 			<P>
 				<FIELDSET>
 					<LEGEND>Datos Personales</LEGEND>
+					
+					<BR/>
 					
 					<LABEL for="nombre">*Tu nombre:</LABEL>
 					<INPUT type="text" name="nombre" size=40 requiered/>
@@ -31,23 +33,34 @@
 
 					<LABEL for="edad">¿Eres mayor o menor de edad?</LABEL>
 					<SELECT>
-						<OPTION value="-18">Tengo menos de 18 años</OPTION>
-						<OPTION value="+18">Tengo más de 18 años</OPTION>
+						<OPTION name="edad" value="-18">Tengo menos de 18 años</OPTION>
+						<OPTION name="edad" value="+18">Tengo más de 18 años</OPTION>
 					</SELECT>
+					
+					<BR/><BR/>
 				</FIELDSET>
 				
 				<BR/><BR/>
 				
 				<FIELDSET>
 					<LEGEND>Sugerencia</LEGEND>
+					
+					<BR/>
+					
+					<TEXTAREA name="sugerencia" requiered cols="40" rows="8" form="FormSugerencia"></TEXTAREA>
+					
+					<BR/>
+					
+					<P>Máximo 300 carácteres</P>
+					
+					<BR/>
 				</FIELDSET>
+				
+				<BR/>
 				
 				<INPUT type="submit" value="Enviar Sugerencia" name="SugerenciaEnviada"/>
 			</P>
 		</FORM>
 	</DIV>
-<?php
-
-?>
 </BODY>
 </HTML>
