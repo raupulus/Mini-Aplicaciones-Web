e9cc6c5284207b53ff3da36c2748afa134a05362
Fryntiz <tecnico@fryntiz.es>
2017-05-26 17:13:00 +0200

    Base para el head

---
+++ Formulario Enviar Sugerencias/procesar.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<HTML lang="es">
+<HEAD>
+	<TITLE>Enviando Sugerencia</TITLE>
+	<META charset="utf-8"/>
+	<META name="description" content="Formulario Sguerencia"/>
+	<META name="keywords" content="formulario, mail, email, recomendar, web, ejemplo, plantilla, programación, Raúl Caro Pastorino, Fryntiz"/>
+	<META name="author" content="Raúl Caro Pastorino"/>
+	<LINK rel="shortcut icon" href="./images/favicon.png"/>
+	<LINK rel="stylesheet" href="./CSS/styles.css"/>
+	<SCRIPT src="JS/scripts.js"></SCRIPT>
+</HEAD>
+
+</HTML>
