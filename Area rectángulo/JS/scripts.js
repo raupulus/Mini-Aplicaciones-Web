73afa24105022236c7f41eab47ef8e1156b7145b
Fryntiz <tecnico@fryntiz.es>
2017-11-05 19:03:03 +0100

    Cabecera de script

--- Area rectángulo/JS/scripts.js
+++ Area rectángulo/JS/scripts.js
@@ -1,6 +1,5 @@
-function calculararea() {
-	var base = document.getElementById('inputbase').value;
-	var altura = document.getElementById('inputaltura').value;
-	var resultado = "El área es:  " + base * altura + " m2";
-	document.getElementById('inputresultado').defaultValue=resultado
-}
+/**
+ * @author Raúl Caro Pastorino
+ * @copyright Copyright © 2017 Raúl Caro Pastorino
+ * @license https://www.gnu.org/licenses/gpl-3.0-standalone.html
+ */
