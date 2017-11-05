15b38f134db780dc225f46d4f28c51313bec646f
Fryntiz <tecnico@fryntiz.es>
2017-11-05 19:33:36 +0100

    Añadida función

--- Area rectángulo/JS/app.js
+++ Area rectángulo/JS/app.js
@@ -4,9 +4,9 @@
  * @license https://www.gnu.org/licenses/gpl-3.0-standalone.html
  */
 
- function calculararea() {
- 	var base = document.getElementById('inputbase').value;
- 	var altura = document.getElementById('inputaltura').value;
- 	var resultado = "El área es:  " + base * altura + " m2";
- 	document.getElementById('inputresultado').defaultValue=resultado
- }
+function calculararea() {
+    var base = document.getElementById('inputbase').value;
+    var altura = document.getElementById('inputaltura').value;
+    var resultado = "El área es:  " + base * altura + " m2";
+    document.getElementById('inputresultado').defaultValue=resultado;
+}
