89f15f64fc32546b64a73a40ec1fff51c1e180bd
Fryntiz <tecnico@fryntiz.es>
2017-11-06 02:55:53 +0100

    Refactorización

--- Conversor de monedas/JS/app.js
+++ Conversor de monedas/JS/app.js
@@ -3,3 +3,19 @@
  * @copyright Copyright © 2017 Raúl Caro Pastorino
  * @license https://www.gnu.org/licenses/gpl-3.0-standalone.html
  */
+
+function calculareuros() {
+    var factor = 166.38;
+    var euros = document.getElementById('inputeuros').value;
+    var pesetas = document.getElementById('inputpesetas').value;
+    var valoreuro = pesetas / factor +" Euros";
+    document.getElementById('inputresultado').defaultValue=valoreuro;
+}
+
+function calcularpesetas() {
+    var factor = 166.38;
+    var euros = document.getElementById('inputeuros').value;
+    var pesetas = document.getElementById('inputpesetas').value;
+    var valorpesetas = euros * factor+" Pesetas";
+    document.getElementById('inputresultado').defaultValue=valorpesetas;
+}
