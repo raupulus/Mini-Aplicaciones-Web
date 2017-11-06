30fc80abe89774c8cd3d7ed9f4d9ada6fd96b693
Fryntiz <tecnico@fryntiz.es>
2017-11-06 01:53:16 +0100

    Refactorizado index2

--- Contador de visitas/index2.php
+++ Contador de visitas/index2.php
@@ -20,32 +20,67 @@
         <script src="JS/app.js"></script>
     </head>
 
-<BODY>
-	<H1 id="titulo1">Contador de visitas</H1>
-	<H2 id="titulo2">Sencillo contador de visitas</H2>
-	<P>Este sencillo contador de visitas se puede utilizar para cada página independiente. Se media a través del archivo "visitas.txt"</P>
+    <body>
+        <div id="cajatitulo">
+            <h1 id="titulo">Contador de visitas</h1>
+            <h2 id="subtitulo">Sencillo contador de visitas</h2>
+        </div>
 
-<!--Contador de visitas de solo una página-->
-	<DIV class="ejemplo">
-		<?php
-		$cuenta = "visitas2.txt";//Donde se almacenan las visitas de esta página
-		function contador($cuenta) {//Función que importa la variable $cuenta
-			$fp = fopen($cuenta, rw);//Abre el archivo con escritura
-			$num = fgets($fp,20);//Obtiene el valor
-			$num += 1;//Suma "1" al valor actual
-			print "Número de visitas: ";
-			echo "$num";
-			exec("rm -rf $cuenta");
-			exec("echo $num > $cuenta");
-		}
-		if(!file_exists($cuenta)) {//Inicia el archivo visitas1.txt si no existe
-			exec("echo 1 > $cuenta");
-		}
-		contador($cuenta);//Ejecuta la función
-		?>
-	</DIV>
-	<BR/>
-	<A href="index.php" title="Principal">Volver a la Web principal</A>
 
-</BODY>
-</HTML>
+        <div id="cajadescripcion">
+            <h3>Modo de uso</h3>
+
+            <p>
+                Este sencillo contador de visitas se puede utilizar para cada página independiente. Se media a través del archivo "visitas.txt"
+            </p>
+        </div>
+
+
+        <div id="cajacontenido">
+            <div id="aplicacion">
+                <!-- Mostrar información -->
+                <div id="mostrarcontador">
+                    <?php
+                        // Importo archivo con las funciones de contadores
+                        include 'funciones.php';
+
+                        // Aumentar Contador
+                        contador($archivos[2]);
+
+                        // Generar estadísticas
+                        visitas();
+                    ?>
+                </div>
+
+                <div id="navegarsitio">
+                    <a href="index.php" title="Principal">
+                        Volver a la Web principal
+                    </a>
+                </div>
+            </div>
+        </div>
+
+
+        <div id="cajafooter">
+            <footer>
+                <p id="autor">
+                    Raúl Caro Pastorino
+                </p>
+
+                <p id="licencia">
+                    Proyecto bajo licencia <a href="https://www.gnu.org/licenses/gpl-3.0-standalone.html" title="Licencia GPLv3" target="_blank">GPLv3</a>
+                    <br />
+                    Licencia libre con reconocimiento de autoría y proyectos derivados bajo las mismas condiciones
+                </p>
+
+                <p id="repositorios">
+                    <a href="https://github.com/fryntiz" title="Repositorios Oficiales de Raúl Caro Pastorino" target="_blank">Repositorios en GitHub Oficial del desarrollador</a>
+                </p>
+
+                <p id="fecha">
+                    <?=date('d-m-Y H:i');?>
+                </p>
+            </footer>
+        </div>
+    </body>
+</html>
