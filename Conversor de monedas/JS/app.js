7a12ff98325ea408a9b5086103c804b092592577
Fryntiz <tecnico@fryntiz.es>
2017-11-06 03:11:22 +0100

    Mejorados estilos para visión correcta

--- Conversor de monedas/JS/app.js
+++ Conversor de monedas/JS/app.js
@@ -3,19 +3,18 @@
  * @copyright Copyright © 2017 Raúl Caro Pastorino
  * @license https://www.gnu.org/licenses/gpl-3.0-standalone.html
  */
+var cambio = 166.38; // Valor de un euro
 
 function calculareuros() {
-    var factor = 166.38;
-    var euros = document.getElementById('inputeuros').value;
     var pesetas = document.getElementById('inputpesetas').value;
-    var valoreuro = pesetas / factor +" Euros";
+    // TOFIX → Limpiar entrada y mostrar alert si no son números, en el caso de venir una coma cambiar por un punto para que sea flotante
+    var valoreuro = pesetas / cambio +" Euros";
     document.getElementById('inputresultado').defaultValue=valoreuro;
 }
 
 function calcularpesetas() {
-    var factor = 166.38;
     var euros = document.getElementById('inputeuros').value;
-    var pesetas = document.getElementById('inputpesetas').value;
-    var valorpesetas = euros * factor+" Pesetas";
+    // TOFIX → Limpiar entrada y mostrar alert si no son números, en el caso de venir una coma cambiar por un punto para que sea flotante
+    var valorpesetas = euros * cambio+" Pesetas";
     document.getElementById('inputresultado').defaultValue=valorpesetas;
 }
