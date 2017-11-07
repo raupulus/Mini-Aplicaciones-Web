07bac9a99915a33bcd9d114caca3f82ff22d7593
Fryntiz <tecnico@fryntiz.es>
2017-11-07 13:20:50 +0100

    Actualizada cabecera

--- Fecha y Hora en la página/index.php
+++ Fecha y Hora en la página/index.php
@@ -1,15 +1,25 @@
 <!DOCTYPE html>
-<HTML lang="es">
-<HEAD>
-    <TITLE>Fecha y Hora</TITLE>
-    <META charset="utf-8"/>
-    <META name="description" content="Plantilla de Ejemplo con fecha y Hora"/>
-    <META name="keywords" content="Fecha, Hora, ejemplo, plantilla, programación, Raúl Caro Pastorino, Fryntiz"/>
-    <META name="author" content="Raúl Caro Pastorino"/>
-    <LINK rel="shortcut icon" href="./images/favicon.png"/>
-    <LINK rel="stylesheet" href="./CSS/styles.css"/>
-    <SCRIPT src="JS/scripts.js"></SCRIPT>
-</HEAD>
+<?php
+/**
+ * @author Raúl Caro Pastorino
+ * @copyright Copyright © 2017 Raúl Caro Pastorino
+ * @license https://www.gnu.org/licenses/gpl-3.0-standalone.html
+ */
+?>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <title>Fecha y Hora</title>
+        <meta name="description" content="Plantilla de Ejemplo con fecha y Hora"/>
+        <meta name="keywords" content="Fecha, Hora, ejemplo, plantilla, programación, Raúl Caro Pastorino, Fryntiz"/>
+        <meta name="author" content="Raúl Caro Pastorino"/>
+        <link rel="shortcut icon" href="./images/favicon.png"/>
+        <link rel="stylesheet" href="./CSS/styles.css"/>
+        <link rel="stylesheet" href="./CSS/app.css"/>
+        <script src="JS/scripts.js"></script>
+        <script src="JS/app.js"></script>
+    </head>
+
 
 <BODY>
     <H1 id="titulo1">FECHA Y HORA</H1>
