e92d018f51ad870e2f03255ac2676ed416b75092
Fryntiz <tecnico@fryntiz.es>
2017-11-08 00:31:17 +0100

    Adaptada estructura a nueva plantilla

--- FirmarTOimagen/index.php
+++ FirmarTOimagen/index.php
@@ -20,27 +20,63 @@
         <script src="JS/app.js"></script>
     </head>
 
-<BODY>
-    <H1 id="titulo1">Firma a Imagen</H1>
-    <H2 id="titulo2">Mini aplicación para firmar</H2>
-    <P>Esta aplicación consta de una capa <STRONG>"CANVAS"</STRONG> donde se recogerá una firma y esta será exportada a una imagen</P>
-    <DIV class="cajaPrincipal">
-        <P>
-            Pulsa en el siguiente título para que se abra el cuadro de firmas:
-        </P>
-        <BR/>
-        <P>
-            <H3 onClick="mostrarLienzo(true)">PULSA AQUÍ PARA FIRMAR</H3>
-        </P>
-        <FORM id="formFirma">
-            <CANVAS id="lienzo"></CANVAS>
-            <BR/>
-            <INPUT type="button" name="limpiar" value="Limpiar" onClick="limpiando()"/>
-            <INPUT type="button" name="guardar" value="Guardar" onClick="guardarImagen()"/>
-        </FORM>
-    </DIV>
-<?php
+    <body>
+        <div id="cajatitulo">
+            <h1 id="titulo">Firma a Imagen</h1>
+            <h2 id="subtitulo">Mini aplicación para firmar</h2>
+        </div>
 
-?>
-</BODY>
-</HTML>
+
+        <div id="cajadescripcion">
+            <h3>Modo de uso</h3>
+
+            <p>
+                Esta aplicación consta de una capa "<strong>CANVAS</strong>" donde se recogerá una firma y esta será exportada a una imagen
+            </p>
+        </div>
+
+
+        <div id="cajacontenido">
+            <div id="aplicacion">
+                <DIV class="cajaPrincipal">
+                    <P>
+                        Pulsa en el siguiente título para que se abra el cuadro de firmas:
+                    </P>
+                    <BR/>
+                    <P>
+                        <H3 id="iniciarFirma" onClick="mostrarLienzo(true)">PULSA AQUÍ PARA FIRMAR</H3>
+                    </P>
+                    <FORM id="formFirma">
+                        <CANVAS id="lienzo"></CANVAS>
+                        <BR/>
+                        <INPUT type="button" name="limpiar" value="Limpiar" onClick="limpiando()"/>
+                        <INPUT type="button" name="guardar" value="Guardar" onClick="guardarImagen()"/>
+                    </FORM>
+                </DIV>
+            </div>
+        </div>
+
+
+        <div id="cajafooter">
+            <footer>
+                <p id="autor">
+                    Raúl Caro Pastorino
+                </p>
+
+                <p id="licencia">
+                    Proyecto bajo licencia <a href="https://www.gnu.org/licenses/gpl-3.0-standalone.html" title="Licencia GPLv3" target="_blank">GPLv3</a>
+                    <br />
+                    Licencia libre con reconocimiento de autoría y proyectos derivados bajo las mismas condiciones
+                </p>
+
+                <p id="repositorios">
+                    <a href="https://github.com/fryntiz" title="Repositorios Oficiales de Raúl Caro Pastorino" target="_blank">Repositorios en GitHub Oficial del desarrollador</a>
+                </p>
+
+                <p id="fecha">
+                    <?=date('d-m-Y H:i');?>
+                </p>
+            </footer>
+        </div>
+    </body>
+</html>
