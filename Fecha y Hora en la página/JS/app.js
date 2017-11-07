4e985af777492f1cd980ab8d21ef29ccc31e74d5
Fryntiz <tecnico@fryntiz.es>
2017-11-07 14:06:31 +0100

    Scripts para recargar tiempo cada minuto planteado

--- Fecha y Hora en la página/JS/app.js
+++ Fecha y Hora en la página/JS/app.js
@@ -3,3 +3,9 @@
  * @copyright Copyright © 2017 Raúl Caro Pastorino
  * @license https://www.gnu.org/licenses/gpl-3.0-standalone.html
  */
+
+
+//Función que recargue la hora cada un tiempo ¿minuto?
+function recargar_hora() {
+
+}
