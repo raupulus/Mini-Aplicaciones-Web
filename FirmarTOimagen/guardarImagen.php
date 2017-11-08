6fb99855a2e1ccef5ee7a1a0a706c8e15426daff
Fryntiz <tecnico@fryntiz.es>
2017-11-08 01:29:33 +0100

    Mejorada estructura

--- FirmarTOimagen/guardarImagen.php
+++ FirmarTOimagen/guardarImagen.php
@@ -1,20 +1,28 @@
 <?php
-  //Leer entrada
-    $data = file_get_contents("php://input");
+/**
+ * @author Raúl Caro Pastorino
+ * @copyright Copyright © 2017 Raúl Caro Pastorino
+ * @license https://www.gnu.org/licenses/gpl-3.0-standalone.html
+ */
 
-    // Filtrar y decodificar los datos pasados
-    // Filtrando las cabeceras (data:,) part.
-    $filteredData=substr($data, strpos($data, ",")+1);
-    // Se necesita decodificar de base64
-    $decodedData=base64_decode($filteredData);
-    // Guardar al servidor
-    $NombreFirma = count(glob("firmas/{*.jpg,*.gif,*.png}",GLOB_BRACE));
-    $NombreFirma = "Firma".($NombreFirma + 1).".png";
-    $fp = fopen('./firmas/'.$NombreFirma, 'wb');
-    $ok = fwrite( $fp, $decodedData);
-    fclose( $fp );
-    if($ok)
-        echo $NombreFirma;
-    else
-        echo "ERROR";
+// Leer entrada
+$data = file_get_contents("php://input");
+
+// Filtrar y decodificar los datos pasados
+// Filtrando las cabeceras (data:,) part.
+$filteredData=substr($data, strpos($data, ",")+1);
+
+// Se necesita decodificar de base64
+$decodedData=base64_decode($filteredData);
+
+// Guardar al servidor
+$NombreFirma = count(glob("firmas/{*.jpg,*.gif,*.png}",GLOB_BRACE));
+$NombreFirma = 'Firma'.($NombreFirma + 1).'.png';
+$fp = fopen('./firmas/'.$NombreFirma, 'wb');
+$ok = fwrite( $fp, $decodedData);
+fclose( $fp );
+if($ok)
+    echo $NombreFirma;
+else
+    echo "ERROR";
 ?>
