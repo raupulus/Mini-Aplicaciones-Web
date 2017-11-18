818245442c3113423e0b3fc9e4b6fb1886d835b7
Fryntiz <tecnico@fryntiz.es>
2017-11-18 18:49:34 +0100

    Añadida cabecera

--- Formulario Enviar Sugerencias/JS/app.js
+++ Formulario Enviar Sugerencias/JS/app.js
@@ -1,3 +1,9 @@
+/**
+ * @author Raúl Caro Pastorino
+ * @copyright Copyright © 2017 Raúl Caro Pastorino
+ * @license https://www.gnu.org/licenses/gpl-3.0-standalone.html
+ */
+
 function validarFormulario() {
     var nombre = document.FormSugerencia.nombre;
     var email = document.FormSugerencia.email;
