4bcc70aecc9c780baad84070cae2ca25124a1de5
Fryntiz <tecnico@fryntiz.es>
2017-11-18 23:02:16 +0100

    New Head

--- Formulario Recomendar WEB por email/procesar.php
+++ Formulario Recomendar WEB por email/procesar.php
@@ -1,15 +1,24 @@
 <!DOCTYPE html>
-<HTML lang="es">
-<HEAD>
-    <TITLE>Formulario recomendar WEB</TITLE>
-    <META charset="utf-8"/>
-    <META name="description" content="Formulario recomendar WEB"/>
-    <META name="keywords" content="formulario, mail, email, recomendar, web, ejemplo, plantilla, programación, Raúl Caro Pastorino, Fryntiz"/>
-    <META name="author" content="Raúl Caro Pastorino"/>
-    <LINK rel="shortcut icon" href="./images/favicon.png"/>
-    <LINK rel="stylesheet" href="./CSS/styles.css"/>
-    <SCRIPT src="JS/scripts.js"></SCRIPT>
-</HEAD>
+<?php
+/**
+ * @author Raúl Caro Pastorino
+ * @copyright Copyright © 2017 Raúl Caro Pastorino
+ * @license https://www.gnu.org/licenses/gpl-3.0-standalone.html
+ */
+?>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <title>Formulario recomendar WEB</title>
+        <meta name="description" content="Formulario recomendar WEB" />
+        <meta name="keywords" content="aformulario, mail, email, recomendar, web, ejemplo, plantilla, programación, Raúl Caro Pastorino, Fryntiz" />
+        <meta name="author" content="Raúl Caro Pastorino" />
+        <link rel="shortcut icon" href="./images/favicon.png" />
+        <link rel="stylesheet" href="./CSS/styles.css" />
+        <link rel="stylesheet" href="./CSS/app.css" />
+        <script src="JS/scripts.js"></script>
+        <script src="JS/app.js"></script>
+    </head>
 
 <BODY>
     <DIV class="ejemplo">
