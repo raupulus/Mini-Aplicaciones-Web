a9a12e6d8fdd017af2656bb92ca0d61584907fd4
Fryntiz <tecnico@fryntiz.es>
2017-11-18 18:35:18 +0100

    Añadida cabecera

--- Formulario Enviar Sugerencias/index.php
+++ Formulario Enviar Sugerencias/index.php
@@ -1,4 +1,11 @@
 <!DOCTYPE html>
+<?php
+/**
+ * @author Raúl Caro Pastorino
+ * @copyright Copyright © 2017 Raúl Caro Pastorino
+ * @license https://www.gnu.org/licenses/gpl-3.0-standalone.html
+ */
+?>
 <HTML lang="es">
 <HEAD>
     <TITLE>Nombre</TITLE>
