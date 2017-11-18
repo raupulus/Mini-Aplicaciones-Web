be4f31f3fba930a404b1b97f5326d18e20ef6850
Fryntiz <tecnico@fryntiz.es>
2017-11-18 18:50:17 +0100

    Limpiadas tabulaciones

--- Formulario Enviar Sugerencias/procesar.php
+++ Formulario Enviar Sugerencias/procesar.php
@@ -1,53 +1,53 @@
 <!DOCTYPE html>
 <HTML lang="es">
 <HEAD>
-	<TITLE>Enviando Sugerencia</TITLE>
-	<META charset="utf-8"/>
-	<META name="description" content="Formulario Sguerencia"/>
-	<META name="keywords" content="formulario, mail, email, recomendar, web, ejemplo, plantilla, programación, Raúl Caro Pastorino, Fryntiz"/>
-	<META name="author" content="Raúl Caro Pastorino"/>
-	<LINK rel="shortcut icon" href="./images/favicon.png"/>
-	<LINK rel="stylesheet" href="./CSS/styles.css"/>
-	<SCRIPT src="JS/scripts.js"></SCRIPT>
+    <TITLE>Enviando Sugerencia</TITLE>
+    <META charset="utf-8"/>
+    <META name="description" content="Formulario Sguerencia"/>
+    <META name="keywords" content="formulario, mail, email, recomendar, web, ejemplo, plantilla, programación, Raúl Caro Pastorino, Fryntiz"/>
+    <META name="author" content="Raúl Caro Pastorino"/>
+    <LINK rel="shortcut icon" href="./images/favicon.png"/>
+    <LINK rel="stylesheet" href="./CSS/styles.css"/>
+    <SCRIPT src="JS/scripts.js"></SCRIPT>
 </HEAD>
 
 <BODY>
-	<DIV class="ejemplo">
-		<?php
-		function limpiarCaracteresEspeciales($string ){
-			$string = htmlentities($string);
-			$string = preg_replace('/\&(.)[^;]*;/', '\\1', $string);
-			//htmlspecialchars($nombre); //Convierte caracteres especiales
-			return $string;
-		}
-		
-		//Variables pasadas mediante POST y filtrada etiquetas HTML
-		$nombre = limpiarCaracteresEspeciales($_POST['nombre']);
-		$email = limpiarCaracteresEspeciales($_POST['email']);
-		$edad = limpiarCaracteresEspeciales($_POST['edad']);
-		$sugerencia = limpiarCaracteresEspeciales($_POST['sugerencia']);
-		
-		//Variables con el Contenido del mensaje
-		$asunto = "Sugerencia";
-		$mensaje = "El usuario <B>$nombre</B>, con email <B>$email</B> ha enviado el siguiente mensaje:\n\n $sugerencia";
+    <DIV class="ejemplo">
+        <?php
+        function limpiarCaracteresEspeciales($string ){
+            $string = htmlentities($string);
+            $string = preg_replace('/\&(.)[^;]*;/', '\\1', $string);
+            //htmlspecialchars($nombre); //Convierte caracteres especiales
+            return $string;
+        }
 
-		//Función debug para comprobar que llegan bien todas las variables
-		function debugMAIL($email,$asunto,$mensaje) {
-			echo "Asunto: $asunto";
-			echo "<BR/>";
-			echo "Email: $email";
-			echo "<BR/>";
-			echo "Mensaje:\n $mensaje";
-		}
+        //Variables pasadas mediante POST y filtrada etiquetas HTML
+        $nombre = limpiarCaracteresEspeciales($_POST['nombre']);
+        $email = limpiarCaracteresEspeciales($_POST['email']);
+        $edad = limpiarCaracteresEspeciales($_POST['edad']);
+        $sugerencia = limpiarCaracteresEspeciales($_POST['sugerencia']);
 
-		debugMAIL($email,$asunto,$mensaje);
-		
-		function enviarMAIL($email,$asunto,$mensaje) {
-			mail($asunto,$mensaje, "FROM: ".$email);
-		}
-		
-		enviarMAIL($email,$asunto,$mensaje);
-		?>
-	</DIV>
+        //Variables con el Contenido del mensaje
+        $asunto = "Sugerencia";
+        $mensaje = "El usuario <B>$nombre</B>, con email <B>$email</B> ha enviado el siguiente mensaje:\n\n $sugerencia";
+
+        //Función debug para comprobar que llegan bien todas las variables
+        function debugMAIL($email,$asunto,$mensaje) {
+            echo "Asunto: $asunto";
+            echo "<BR/>";
+            echo "Email: $email";
+            echo "<BR/>";
+            echo "Mensaje:\n $mensaje";
+        }
+
+        debugMAIL($email,$asunto,$mensaje);
+
+        function enviarMAIL($email,$asunto,$mensaje) {
+            mail($asunto,$mensaje, "FROM: ".$email);
+        }
+
+        enviarMAIL($email,$asunto,$mensaje);
+        ?>
+    </DIV>
 </BODY>
 </HTML>
