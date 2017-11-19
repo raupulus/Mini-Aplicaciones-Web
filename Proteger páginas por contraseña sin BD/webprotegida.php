e263774fdaa671351eaf6c5fe5c050c4841e413e
Fryntiz <tecnico@fryntiz.es>
2017-11-19 16:42:49 +0100

    Limpieza de tabs

--- Proteger páginas por contraseña sin BD/webprotegida.php
+++ Proteger páginas por contraseña sin BD/webprotegida.php
@@ -1,24 +1,24 @@
 <!DOCTYPE html>
 <HTML lang="es">
 <HEAD>
-	<TITLE>Proteger página por contraseña</TITLE>
-	<META charset="utf-8"/>
-	<META name="description" content="Proteger página por contraseña"/>
-	<META name="keywords" content="proteger, contraseña, pagina, proteger pagina, ejemplo, plantilla, programación, Raúl Caro Pastorino, Fryntiz"/>
-	<META name="author" content="Raúl Caro Pastorino"/>
-	<LINK rel="shortcut icon" href="./images/favicon.png"/>
-	<LINK rel="stylesheet" href="./CSS/styles.css"/>
-	<SCRIPT src="JS/scripts.js"></SCRIPT>
+    <TITLE>Proteger página por contraseña</TITLE>
+    <META charset="utf-8"/>
+    <META name="description" content="Proteger página por contraseña"/>
+    <META name="keywords" content="proteger, contraseña, pagina, proteger pagina, ejemplo, plantilla, programación, Raúl Caro Pastorino, Fryntiz"/>
+    <META name="author" content="Raúl Caro Pastorino"/>
+    <LINK rel="shortcut icon" href="./images/favicon.png"/>
+    <LINK rel="stylesheet" href="./CSS/styles.css"/>
+    <SCRIPT src="JS/scripts.js"></SCRIPT>
 </HEAD>
 
 <BODY>
-	<H1 id="titulo1">Proteger página por contraseña</H1>
-	<H2 id="titulo2">Acceder a un sitio web utilizando contraseña</H2>
-	<P>Esta aplicación trata de poner una contraseña que te obliga a identificarte para restringir el acceso hacia un sitio web</P>
-	<DIV class="ejemplo">
-		Esta es la WEB protegida.
-		<BR/>
-		Redireccionamiento completado.
-	</DIV>
+    <H1 id="titulo1">Proteger página por contraseña</H1>
+    <H2 id="titulo2">Acceder a un sitio web utilizando contraseña</H2>
+    <P>Esta aplicación trata de poner una contraseña que te obliga a identificarte para restringir el acceso hacia un sitio web</P>
+    <DIV class="ejemplo">
+        Esta es la WEB protegida.
+        <BR/>
+        Redireccionamiento completado.
+    </DIV>
 </BODY>
 </HTML>
