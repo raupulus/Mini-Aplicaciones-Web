627d58eab9d150df06d084118e8c9cbe0eab2777
Fryntiz <tecnico@fryntiz.es>
2017-11-19 23:05:35 +0100

    Instrucciones refactorizadas en funciones

--- Tiempo para cargar la Página/funciones.php
+++ Tiempo para cargar la Página/funciones.php
@@ -1,5 +1,10 @@
 <?php
+$tiempoinicial = 0;
+$tiempofinal = 0;
+
 function iniciar_contador() {
+    global $tiempoinicial;
+
     $tiempo = microtime();
     $tiempo = explode(" ",$tiempo);
     $tiempo = $tiempo[1] + $tiempo[0];
@@ -7,9 +12,19 @@ function iniciar_contador() {
 }
 
 function finalizar_contador() {
+    global $tiempofinal;
+
     $tiempo = microtime();
     $tiempo = explode(" ",$tiempo);
     $tiempo = $tiempo[1] + $tiempo[0];
     $tiempofinal = $tiempo;
+}
+
+function mostrar_tiempo() {
+    global $tiempofinal;
+    global $tiempoinicial;
+
+    $tiempototal = number_format($tiempofinal - $tiempoinicial,20);
+    return $tiempototal;
 }
 ?>
