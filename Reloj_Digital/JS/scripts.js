662f6bd1745035230a9db745ef7aac253ff34497
Fryntiz <tecnico@fryntiz.es>
2017-11-19 22:18:33 +0100

    Añadida cabecera

--- Reloj_Digital/JS/scripts.js
+++ Reloj_Digital/JS/scripts.js
@@ -1,3 +1,10 @@
+/**
+ * @author Raúl Caro Pastorino
+ * @copyright Copyright © 2017 Raúl Caro Pastorino
+ * @license https://www.gnu.org/licenses/gpl-3.0-standalone.html
+ */
+
+
 //Función reloj
 function cambiarHora() {
     momentoActual = new Date();//Crea hora
