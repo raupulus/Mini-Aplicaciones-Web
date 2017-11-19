108ed1b05fbb7f20d02e28b9e06750b9f76aebdd
Fryntiz <tecnico@fryntiz.es>
2017-11-19 21:55:32 +0100

    Tabs fix

--- Reloj_Digital/index.php
+++ Reloj_Digital/index.php
@@ -1,27 +1,27 @@
 <!DOCTYPE html>
 <HTML lang="es">
 <HEAD>
-	<TITLE>Reloj Digital</TITLE>
-	<META charset="utf-8"/>
-	<META name="description" content="Mostrar tiempo de carga de la página"/>
-	<META name="keywords" content="reloj, digital, hora, tiempo, ejemplo, plantilla, programación, Raúl Caro Pastorino, Fryntiz"/>
-	<META name="author" content="Raúl Caro Pastorino"/>
-	<LINK rel="shortcut icon" href="./images/favicon.png"/>
-	<LINK rel="stylesheet" href="./CSS/styles.css"/>
-	<SCRIPT src="JS/scripts.js"></SCRIPT>
+    <TITLE>Reloj Digital</TITLE>
+    <META charset="utf-8"/>
+    <META name="description" content="Mostrar tiempo de carga de la página"/>
+    <META name="keywords" content="reloj, digital, hora, tiempo, ejemplo, plantilla, programación, Raúl Caro Pastorino, Fryntiz"/>
+    <META name="author" content="Raúl Caro Pastorino"/>
+    <LINK rel="shortcut icon" href="./images/favicon.png"/>
+    <LINK rel="stylesheet" href="./CSS/styles.css"/>
+    <SCRIPT src="JS/scripts.js"></SCRIPT>
 </HEAD>
 
 <BODY onload="cambiarHora()"><!--Funcion JS al cargar página-->
-	<H1>Reloj Digital</H1>
-	<H2>Aplicación que contiene un reloj Digital</H2>
-	<P>La hora que establece este reloj será tomada desde el ordenador del cliente (No desde el servidor)</P>
+    <H1>Reloj Digital</H1>
+    <H2>Aplicación que contiene un reloj Digital</H2>
+    <P>La hora que establece este reloj será tomada desde el ordenador del cliente (No desde el servidor)</P>
 
 <!--Caja que contiene el reloj-->
-	<DIV id="CajaReloj">
-		<P id="hora">
-			HH:MM:SS
-		</P>
-	</DIV>
+    <DIV id="CajaReloj">
+        <P id="hora">
+            HH:MM:SS
+        </P>
+    </DIV>
 <!--Fin del reloj-->
 </BODY>
 </HTML>
