8773549fe705bac5ba9a47699b9c1d241b2a7004
Fryntiz <tecnico@fryntiz.es>
2017-11-19 16:20:44 +0100

    Mejorada estructura

--- Proteger páginas por contraseña sin BD/comprobar.php
+++ Proteger páginas por contraseña sin BD/comprobar.php
@@ -3,7 +3,11 @@ $usuario = filter_input(INPUT_POST, 'usuario');
 $pass = filter_input(INPUT_POST, 'pass');
 
 if ($usuario === 'usuario' && $pass === 'clave') {
-    echo "<SCRIPT type='text/javascript'>alert('Bienvenido, has introducido correctamente el usuario y la contraseña.')</SCRIPT>";
+?>
+    <SCRIPT type='text/javascript'>
+        alert('Bienvenido, has introducido correctamente el usuario y la contraseña.')
+    </SCRIPT>
+<?php
     //header("Location: webprotegida.php");
     include "webprotegida.php";
 } else {
