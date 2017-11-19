d726ba2827df8974bd42a5715a10097b99676df7
Fryntiz <tecnico@fryntiz.es>
2017-11-19 22:18:12 +0100

    fix tabs

--- Reloj_Digital/JS/scripts.js
+++ Reloj_Digital/JS/scripts.js
@@ -1,25 +1,25 @@
 //Función reloj
 function cambiarHora() {
-	momentoActual = new Date();//Crea hora
-	reloj = document.getElementById('hora');//id donde pintar
-	hora = momentoActual.getHours();//Extraemos Hora
-	minutos = momentoActual.getMinutes();//Extraemos Minutos
-	segundos = momentoActual.getSeconds();//Extraemos Segundos
+    momentoActual = new Date();//Crea hora
+    reloj = document.getElementById('hora');//id donde pintar
+    hora = momentoActual.getHours();//Extraemos Hora
+    minutos = momentoActual.getMinutes();//Extraemos Minutos
+    segundos = momentoActual.getSeconds();//Extraemos Segundos
 
 //Comprobamos si es menor de 10 que añada 0 por estética
-	if (hora < '10') {
-		hora = "0" + hora;
-	}
-	if (minutos < '10') {
-		minutos = "0" + minutos;
-	}
-	if (segundos < '10') {
-		segundos = "0" + segundos;
-	}
+    if (hora < '10') {
+        hora = "0" + hora;
+    }
+    if (minutos < '10') {
+        minutos = "0" + minutos;
+    }
+    if (segundos < '10') {
+        segundos = "0" + segundos;
+    }
 
 //Pintamos el resultado dentro del reloj
-	reloj.innerHTML = hora + " : " + minutos + " : " + segundos;
+    reloj.innerHTML = hora + " : " + minutos + " : " + segundos;
 
-	//Actualizar reloj cada segundo
-	setTimeout("cambiarHora()",1000);
+    //Actualizar reloj cada segundo
+    setTimeout("cambiarHora()",1000);
 }
