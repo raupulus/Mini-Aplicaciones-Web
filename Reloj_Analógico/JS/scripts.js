ce8b3e122bc10b555c26efd76b6d1c185841b5f8
Fryntiz <tecnico@fryntiz.es>
2017-11-19 20:25:05 +0100

    fix tabs

--- Reloj_Analógico/JS/scripts.js
+++ Reloj_Analógico/JS/scripts.js
@@ -1,35 +1,35 @@
 function funcionReloj() {
-	var momentoActual = new Date();//Crea hora
-	var horaActual = momentoActual.getHours();//Extraemos Hora
-	var minutoActual = momentoActual.getMinutes();//Extraemos Minutos
-	var segundoActual = momentoActual.getSeconds();//Extraemos Segundos
+    var momentoActual = new Date();//Crea hora
+    var horaActual = momentoActual.getHours();//Extraemos Hora
+    var minutoActual = momentoActual.getMinutes();//Extraemos Minutos
+    var segundoActual = momentoActual.getSeconds();//Extraemos Segundos
 
 //Identificadores para elementos
-	var hora = document.getElementById('hora');
-	var minutos = document.getElementById('minutos');
-	var segundos = document.getElementById('segundos');
+    var hora = document.getElementById('hora');
+    var minutos = document.getElementById('minutos');
+    var segundos = document.getElementById('segundos');
 
-	function cambiarHora() {
-	//Calcular grados por hora (30deg cada hora, 30*12=360º)
-		var horaActualGrados = horaActual * 30 + "deg";
-		hora.style.transform = "rotate(" + horaActualGrados + ")";
+    function cambiarHora() {
+    //Calcular grados por hora (30deg cada hora, 30*12=360º)
+        var horaActualGrados = horaActual * 30 + "deg";
+        hora.style.transform = "rotate(" + horaActualGrados + ")";
 
-	//Calcular grados cada minuto (6deg cada minuto, 6*6=360º)
-		var minutoActualGrados = minutoActual * 6 + "deg";
-		minutos.style.transform = "rotate(" + minutoActualGrados + ")";
-	}
+    //Calcular grados cada minuto (6deg cada minuto, 6*6=360º)
+        var minutoActualGrados = minutoActual * 6 + "deg";
+        minutos.style.transform = "rotate(" + minutoActualGrados + ")";
+    }
 
-	function cambiarSegundos() {
-	//Calcular grados cada segundo (6deg cada segundo, 6*6=360º)
-		var segundoActualGrados = segundoActual * 6 + "deg";
-		segundos.style.transform = "rotate(" + segundoActualGrados + ")";
-	}
+    function cambiarSegundos() {
+    //Calcular grados cada segundo (6deg cada segundo, 6*6=360º)
+        var segundoActualGrados = segundoActual * 6 + "deg";
+        segundos.style.transform = "rotate(" + segundoActualGrados + ")";
+    }
 
 cambiarHora();
 cambiarSegundos();
 
 //Actualizar reloj
-	setTimeout("funcionReloj()",1000);
+    setTimeout("funcionReloj()",1000);
 //Actualizar reloj cada segundo
-//	setTimeout("cambiarSegundos()",1000);
+//    setTimeout("cambiarSegundos()",1000);
 }
