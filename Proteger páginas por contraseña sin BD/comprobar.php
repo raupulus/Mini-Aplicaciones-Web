2c25fb78e645c2fda4f3c2f4ef321e612cad0661
Fryntiz <tecnico@fryntiz.es>
2017-11-19 16:18:54 +0100

    Mejorada estructura y saneamiento

--- Proteger páginas por contraseña sin BD/comprobar.php
+++ Proteger páginas por contraseña sin BD/comprobar.php
@@ -1,18 +1,13 @@
-        <?php
-        $usuario = $_POST['usuario'];
-        $pass = $_POST['pass'];
+<?php
+$usuario = filter_input(INPUT_POST, 'usuario');
+$pass = filter_input(INPUT_POST, 'pass');
 
-        if($usuario=="usuario" && $pass=="clave") {
-            $valido = "si";
-        } else {
-            $valido = "no";
-        }
-        if ($valido == "si") {
-            echo "<SCRIPT type='text/javascript'>alert('Bienvenido, has introducido correctamente el usuario y la contraseña.')</SCRIPT>";
-            //header("Location: webprotegida.php");
-            include "webprotegida.php";
-        } else {
-            //echo "Vuelve a intentarlo:<BR/>El usuario o la contraseña son incorrectos.";
-            include "error.php";
-        }
-        ?>
+if ($usuario === 'usuario' && $pass === 'clave') {
+    echo "<SCRIPT type='text/javascript'>alert('Bienvenido, has introducido correctamente el usuario y la contraseña.')</SCRIPT>";
+    //header("Location: webprotegida.php");
+    include "webprotegida.php";
+} else {
+    //echo "Vuelve a intentarlo:<BR/>El usuario o la contraseña son incorrectos.";
+    include "error.php";
+}
+?>
