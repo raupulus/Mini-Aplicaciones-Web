8af53b9da7b58b01bcaab329c7ba48db54aa58c2
Fryntiz <tecnico@fryntiz.es>
2017-11-19 22:00:13 +0100

    Reestructurado head

--- Reloj_Digital/index.php
+++ Reloj_Digital/index.php
@@ -1,15 +1,24 @@
 <!DOCTYPE html>
-<HTML lang="es">
-<HEAD>
-    <TITLE>Reloj Digital</TITLE>
-    <META charset="utf-8"/>
-    <META name="description" content="Mostrar tiempo de carga de la página"/>
-    <META name="keywords" content="reloj, digital, hora, tiempo, ejemplo, plantilla, programación, Raúl Caro Pastorino, Fryntiz"/>
-    <META name="author" content="Raúl Caro Pastorino"/>
-    <LINK rel="shortcut icon" href="./images/favicon.png"/>
-    <LINK rel="stylesheet" href="./CSS/styles.css"/>
-    <SCRIPT src="JS/scripts.js"></SCRIPT>
-</HEAD>
+<?php
+/**
+ * @author Raúl Caro Pastorino
+ * @copyright Copyright © 2017 Raúl Caro Pastorino
+ * @license https://www.gnu.org/licenses/gpl-3.0-standalone.html
+ */
+?>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <title>Reloj Digital</title>
+        <meta name="description" content="Reloj Digital" />
+        <meta name="keywords" content="reloj, digital, hora, tiempo, ejemplo, plantilla, programación, Raúl Caro Pastorino, Fryntiz" />
+        <meta name="author" content="Raúl Caro Pastorino" />
+        <link rel="shortcut icon" href="./images/favicon.png" />
+        <link rel="stylesheet" href="./CSS/styles.css" />
+        <link rel="stylesheet" href="./CSS/app.css" />
+        <script src="JS/scripts.js"></script>
+        <script src="JS/app.js"></script>
+    </head>
 
 <BODY onload="cambiarHora()"><!--Funcion JS al cargar página-->
     <H1>Reloj Digital</H1>
