480581ace59acfe5f8dcae12e78f48498dc1b0a5
Fryntiz <tecnico@fryntiz.es>
2017-11-19 01:38:12 +0100

    Contenido reestructurado

--- Frases aleatorias con cada carga de la página/index.php
+++ Frases aleatorias con cada carga de la página/index.php
@@ -38,42 +38,44 @@
 
         <div id="cajacontenido">
             <div id="aplicacion">
+                <DIV>
+                    <?php
+                    $frases = array(//Creo array con frases
+                        1=>"Hola navegante.",
+                        2=>"Bienvenido a mi web.",
+                        3=>"Gracias por visitarnos.",
+                        4=>"Te recomiendo visitar nuestro foro.",
+                        5=>"Puedes enviarnos las sugerencias que quieras.",
+                        6=>"No dejes de visitarnos estos días, tendremos nuevas sorpresas.",
+                    );
+                    $aleatorio = rand(1,6);//Crea un número aleatorio entre 1 y 6
+                    echo "La frase aleatoria del momento es: <BR/>";
+                    echo "<B style='color:red; font-size:2em;'>$frases[$aleatorio]</B>";
+                    echo "<BR/><BR/>";
+                    ?>
+                </DIV>
 
+                <DIV>
+                    <?php
+                    //Aquí creo bucle para mostrar todo el array con las frases y se pueda ver las que existen
+                    echo "<BR/><BR/>";
+                    echo "<H3 style='font-size:2.2em; text-align:center;'>Listado de frases disponibles:</H3>";
+                    $i = 1;
+                    $limite = count($frases);
+                    while ($i<=$limite) {
+                        echo "<B style=color:red>".$i." - </B>";
+                        echo $frases[$i];
+                        echo "<BR/>";
+                        $i++;
+                    }
+                    ?>
+                </DIV>
             </div>
         </div>
 
 
-    <DIV class="ejemplo">
-        <?php
-        $frases = array(//Creo array con frases
-            1=>"Hola navegante.",
-            2=>"Bienvenido a mi web.",
-            3=>"Gracias por visitarnos.",
-            4=>"Te recomiendo visitar nuestro foro.",
-            5=>"Puedes enviarnos las sugerencias que quieras.",
-            6=>"No dejes de visitarnos estos días, tendremos nuevas sorpresas.",
-        );
-        $aleatorio = rand(1,6);//Crea un número aleatorio entre 1 y 6
-        echo "La frase aleatoria del momento es: <BR/>";
-        echo "<B style='color:red; font-size:2em;'>$frases[$aleatorio]</B>";
-        echo "<BR/><BR/>";
-        ?>
-    </DIV>
 
-    <DIV style="color:yellow;">
-        <?php
-        //Aquí creo bucle para mostrar todo el array con las frases y se pueda ver las que existen
-        echo "<BR/><BR/>";
-        echo "<H3 style='font-size:2.2em; text-align:center;'>Listado de frases disponibles:</H3>";
-        $i = 1;
-        $limite = count($frases);
-        while ($i<=$limite) {
-            echo "<B style=color:red>".$i." - </B>";
-            echo $frases[$i];
-            echo "<BR/>";
-            $i++;
-        }
-        ?>
-    </DIV>
+
+
 </BODY>
 </HTML>
