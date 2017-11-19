1f0ed361d3e81acbc0335e41db83b2cd914ebbfc
Fryntiz <tecnico@fryntiz.es>
2017-11-19 23:06:37 +0100

    Añadido footer

--- Tiempo para cargar la Página/index.php
+++ Tiempo para cargar la Página/index.php
@@ -58,5 +58,28 @@
 
             </div>
         </div>
-</BODY>
-</HTML>
+
+
+        <div id="cajafooter">
+            <footer>
+                <p id="autor">
+                    Raúl Caro Pastorino
+                </p>
+
+                <p id="licencia">
+                    Proyecto bajo licencia <a href="https://www.gnu.org/licenses/gpl-3.0-standalone.html" title="Licencia GPLv3" target="_blank">GPLv3</a>
+                    <br />
+                    Licencia libre con reconocimiento de autoría y proyectos derivados bajo las mismas condiciones
+                </p>
+
+                <p id="repositorios">
+                    <a href="https://github.com/fryntiz" title="Repositorios Oficiales de Raúl Caro Pastorino" target="_blank">Repositorios en GitHub Oficial del desarrollador</a>
+                </p>
+
+                <p id="fecha">
+                    <?=date('d-m-Y H:i');?>
+                </p>
+            </footer>
+        </div>
+    </body>
+</html>
